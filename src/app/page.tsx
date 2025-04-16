0e1deebd1f00062f1a77ca694107a4fe8360d81d
Aweroro Hadi <hadistic20@gmail.com>
2025-04-16 18:44:05 +0100

    added wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,19 @@ import FeaturedLogosSection from "./components/FeaturedLogosSection";
 import CardIntro from "./components/CardIntro";
 import CardFeatures from "./components/CardFeatures";
 import Footer from "./components/Footer";
+import MaxWidthWrapper from "./components/MaxWidthWrapper";
 
 export default function Home() {
   return (
     <div className="p-4 bg-black overflow-hidden">
-      <Header/>
-      <HeroSection/>
-      <FeaturedLogosSection/>
-      <CardIntro/>
-      <CardFeatures/>
-      <Footer/>
+      <MaxWidthWrapper>
+        <Header/>
+        <HeroSection/>
+        <FeaturedLogosSection/>
+        <CardIntro/>
+        <CardFeatures/>
+        <Footer/>
+      </MaxWidthWrapper>
     </div>
   );
 }
